98bdbbf09e256d51a0f601c77d086bbd9498ebca
Noah <greffnoah@gmail.com>
2021-03-18 16:19:55 +0100

    updated to work with custom locations

--- api.js
+++ api.js
@@ -3,21 +3,23 @@ const axios = require('axios');
 const router = express.Router();
 module.exports = router;
 
-const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?aggregateHours=24&combinationMethod=aggregate&startDateTime=2017-01-01T00%3A00%3A00&endDateTime=2021-01-01T00%3A00%3A00&maxStations=-1&maxDistance=-1&contentType=json&unitGroup=metric&locationMode=single&key=21C8DJHWCKLUKNSPTDEDNA3AW&dataElements=default&locations=Belgium"
-const _url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Netherlands?unitGroup=metric&key=ASR7X3K6EKCFLNE85J9VG5E4C";
 
-router.get('/info/basic', async(req, res) => {
+router.get('/location/:loc', async(req, res) => {
+
+    const loc = req.param.loc | 'Netherlands';
+    const url = getUrlWithLocation(loc)
 
     try {
         const data = await axios.get(url);
-
-        console.log(data.data);
-
         res.json(data.data);
     }
     catch (error) {
         console.log(error)
         res.sendStatus(500);
     }
-
 });
+
+
+function getUrlWithLocation(location) {
+    return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/history?aggregateHours=24&combinationMethod=aggregate&startDateTime=2017-06-01T00%3A00%3A00&endDateTime=2021-01-01T00%3A00%3A00&maxStations=-1&maxDistance=-1&contentType=json&unitGroup=metric&locationMode=single&key=21C8DJHWCKLUKNSPTDEDNA3AW&dataElements=default&locations=${location}`;
+}
